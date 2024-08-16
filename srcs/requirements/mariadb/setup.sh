18924ecb511430162403950af44cec7c5e846149
Dilshod Davletyarov <dilshod95@gmail.com>
2024-08-16 12:42:04 +0200

    wip

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-service mysql start
+mkdir -p /var/lib/mysql/
 
+chown -R mysql:mysql /var/lib/mysql
+
+# chown -R mysql:mysql /var/lib/mysql
+# service mysql start
+mysqld
+# /etc/init.d/mariadb setup
+# rc-service mariadb start
 # Define SQL commands for initialization
-cat<<EOF > /tmp/db.sql
+cat << EOF > /tmp/db.sql
 CREATE DATABASE IF NOT EXISTS ${DB_NAME} ;
 CREATE USER IF NOT EXISTS '${DB_ADMN}'@'%' IDENTIFIED BY '${DB_ADMNPWD}' ;
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_ADMN}'@'%' ;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USRPWD}' ;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ${DB_NAME}.* TO '${DB_USER}'@'%' ;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_RTPWD}' ;
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES ;
 EOF
 # Execute SQL commands
 mysql < /tmp/db.sql
@@ -18,4 +25,4 @@ mysql < /tmp/db.sql
 # service mysql stop
 # Restart MySQL service
 # service mysql start
-service mariadb restart
+# rc-service mariadb restart
