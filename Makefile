2908d1c061c97bb2d071eccd9a35ed3e03222703
ddavlet <ddavlet@inception.inception.ddavlety.com>
2024-08-25 20:39:17 +0200

    mand part done without checking

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: up
 
 up:
-	mkdir -p /Users/${USER}/data/wordpress
-	mkdir -p /Users/${USER}/data/mariadb
-	chmod -R 777 /Users/${USER}/data/
+	mkdir -p /home/${USER}/data/wordpress
+	mkdir -p /home/${USER}/data/mariadb
+	chmod -R 777 /home/${USER}/data/
 	docker compose -f srcs/docker-compose.yml up --build
 down:
 	docker compose -f srcs/docker-compose.yml down
@@ -12,10 +12,12 @@ clean: down
 	# docker volume prune -af
 	# docker volume rm wp-volume
 	# docker volume rm db-volume
-	# rm -rf /Users/${USER}/data
+	# rm -rf /home/${USER}/data
 
 fclean: down clean
-	docker volume prune -af
-	rm -rf /Users/${USER}/data
+	# docker volume prune -af
+	# docker volume rm wp-volume
+	# docker volume rm db-volume
+	rm -rf /home/${USER}/data
 
 re: fclean up
--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 
-#if mysql (settings folder for mariadb) is not installed
+# Load database credentials from the secrets file
+. /run/secrets/db-secrets
+
+# Check if MySQL/MariaDB is not installed
 if [ ! -d "/var/lib/mysql/ibdata1" ]; then
-    #init mariadb settings, --rmp install mode for linux based systems
+    # Initialize MariaDB settings
     mysql_install_db --datadir=/var/lib/mysql --user=mysql
 fi
 
-# Start MariaDB on background + wait to start
-/usr/bin/mysqld_safe --datadir=/var/lib/mysql --user=mysql &
-sleep 5  # Wait for MariaDB to fully start
+# Start MariaDB in the background and wait for it to fully start
+/usr/bin/mysqld_safe --skip-log-error --datadir=/var/lib/mysql --user=mysql &
+sleep 5
 
-
-if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
-    cat << EOF > /tmp/db.sql
+# Create SQL commands file
+cat << EOF > /tmp/db.sql
 # Delete test database
 DELETE FROM mysql.user WHERE User = '';
 DROP DATABASE IF EXISTS test;
-CREATE DATABASE IF NOT EXISTS ${DB_NAME} ;
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USRPWD}' ;
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' ;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_RTPWD}' ;
-FLUSH PRIVILEGES ;
+CREATE DATABASE IF NOT EXISTS ${database_name};
+CREATE USER IF NOT EXISTS '${user_name}'@'%' IDENTIFIED BY '${user_password}';
+GRANT ALL PRIVILEGES ON ${database_name}.* TO '${user_name}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${root_password}';
+FLUSH PRIVILEGES;
 EOF
+
 # Execute SQL commands
 mysql < /tmp/db.sql
+
+# Clean up
 rm -f /tmp/db.sql
-fi
-# Stop MySQL service
-# service mysql stop
-# Restart MySQL service
-# service mysql start
-# rc-service mariadb restart
-# exec mysqld_safe --datadir=/var/lib/mysql
+
+# Restart MariaDB
+
+# Wait for MariaDB to fully start
+wait
